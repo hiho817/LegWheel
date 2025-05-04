b3be0fabb062c199a3a2c46fc0dbcc5435fee954
kyle1548 <kyle9296@gmail.com>
2025-05-04 21:46:14 +0800

    now can set link_alpha so link can be not transparent. move function of set_shape to get_shape and remove set_shape (e.i. +O to all elements)

--- PlotLeg.py
+++ PlotLeg.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Arc
+from matplotlib.patches import Circle
+from matplotlib.patches import Wedge
 from matplotlib.lines import Line2D
 import time
 import LegModel
@@ -27,14 +29,17 @@ class PlotLeg(LegModel.LegModel):
             self.line_width = 1.0
             self.zorder = 1.0
             self.color = "black"
-            self.get_shape()
+            self.link_alpha = 0.0
+            self.get_shape(O)
             
         class rim:
-            def __init__(self, arc, arc_out, start):
+            def __init__(self, arc, arc_out, start, arc_fill):
                 self.arc = [arc, arc_out]   # inner & outer arcs
                 self.start = start          # start angle 
+                self.arc_fill = arc_fill    # fill white between inner & outer arcs
                 
-        def get_shape(self):
+        def get_shape(self, O):
+            self.O = np.array(O)    # origin of leg in world coordinate
             # four rims (inner arc, outer arc, start angle)
             self.upper_rim_r = self.rim( *self.get_arc(self.leg_model.F_r, self.leg_model.H_r, self.leg_model.U_r, self.r))
             self.upper_rim_l = self.rim( *self.get_arc(self.leg_model.H_l, self.leg_model.F_l, self.leg_model.U_l, self.r))
@@ -58,84 +63,56 @@ class PlotLeg(LegModel.LegModel):
             start = np.angle(p1-o, deg=True)
             end = np.angle(p2-o, deg=True)
             radius = np.abs(p1-o)
-            arc = Arc([o.real, o.imag], 2*(radius-offset), 2*(radius-offset), angle=0.0, theta1=start, theta2=end, color=self.color, linewidth=self.line_width, zorder=self.zorder)
-            arc_out = Arc([o.real, o.imag], 2*(radius+offset), 2*(radius+offset), angle=0.0, theta1=start, theta2=end, color=self.color, linewidth=self.line_width, zorder=self.zorder)
-            return arc, arc_out, start
+            arc = Arc(self.O+[o.real, o.imag], 2*(radius-offset), 2*(radius-offset), angle=0.0, theta1=start, theta2=end, color=self.color, linewidth=self.line_width, zorder=self.zorder)
+            arc_out = Arc(self.O+[o.real, o.imag], 2*(radius+offset), 2*(radius+offset), angle=0.0, theta1=start, theta2=end, color=self.color, linewidth=self.line_width, zorder=self.zorder)
+            arc_fill = Wedge(center=self.O+[o.real, o.imag], r=(radius+offset), width=2*offset, theta1=start, theta2=end, facecolor=(1, 1, 1, self.link_alpha), zorder=self.zorder-0.00001)
+            return arc, arc_out, start, arc_fill
 
         def get_circle(self, o, r):
-            circle = Arc([o.real, o.imag], 2*r, 2*r, angle=0.0, theta1=0, theta2=360, color=self.color, linewidth=self.line_width, zorder=self.zorder)
+            circle = Circle(self.O+[o.real, o.imag], radius=r, facecolor=(1, 1, 1, self.link_alpha), edgecolor=self.color, linewidth=self.line_width, zorder=self.zorder)
+            # circle = Arc([o.real, o.imag], 2*r, 2*r, angle=0.0, theta1=0, theta2=360, color=self.color, linewidth=self.line_width, zorder=self.zorder)
             return circle
 
         def get_line(self, p1, p2):
-            line = Line2D([p1.real, p2.real], [p1.imag, p2.imag], marker='o', markersize=self.mark_size, linestyle='-', color=self.color, linewidth=self.line_width, zorder=self.zorder)
+            line = Line2D(self.O[0]+[p1.real, p2.real], self.O[1]+[p1.imag, p2.imag], marker='o', markersize=self.mark_size, linestyle='-', color=self.color, linewidth=self.line_width, zorder=self.zorder)
             return line
         
-        ## Set Postion Of Leg ##  
-        def set_shape(self, O):
-            self.O = np.array(O)    # origin of leg in world coordinate
-            # four rims (rim, start point, center)
-            self.set_rim(self.upper_rim_r, self.leg_model.F_r, self.leg_model.U_r)
-            self.set_rim(self.upper_rim_l, self.leg_model.H_l, self.leg_model.U_l)
-            self.set_rim(self.lower_rim_r, self.leg_model.G, self.leg_model.L_r)
-            self.set_rim(self.lower_rim_l, self.leg_model.F_l, self.leg_model.L_l)
-            # five joints on the rims   (joint, center)
-            self.set_joint(self.upper_joint_r, self.leg_model.H_r)
-            self.set_joint(self.upper_joint_l, self.leg_model.H_l)
-            self.set_joint(self.lower_joint_r, self.leg_model.F_r)
-            self.set_joint(self.lower_joint_l, self.leg_model.F_l)
-            self.set_joint(self.G_joint, self.leg_model.G)
-            # six bars  (bar, point1, point2)
-            self.set_bar(self.OB_bar_r, 0, self.leg_model.B_r)
-            self.set_bar(self.OB_bar_l, 0, self.leg_model.B_l)
-            self.set_bar(self.AE_bar_r, self.leg_model.A_r, self.leg_model.E)
-            self.set_bar(self.AE_bar_l, self.leg_model.A_l, self.leg_model.E)
-            self.set_bar(self.CD_bar_r, self.leg_model.C_r, self.leg_model.D_r)
-            self.set_bar(self.CD_bar_l, self.leg_model.C_l, self.leg_model.D_l)
-            
-        def set_rim(self, rim, p1, o):  # rim, start point, center
-            start = np.angle(p1-o, deg=True) 
-            for arc in rim.arc: # inner & outer arcs
-                arc.set_center([o.real, o.imag] + self.O)    # center(x, y)
-                arc.set_angle( start - rim.start )    # rotate angle (degree)
-                
-        def set_joint(self, joint, center): # joint, center
-            joint.set_center([center.real, center.imag] + self.O)
-            
-        def set_bar(self, bar, p1, p2): # bar, point1, point2
-            bar.set_data([p1.real, p2.real] + self.O[0], [p1.imag, p2.imag] + self.O[1])
-
         
     ## Parameters Setting ##
-    def setting(self, fig_size=-1, mark_size=-1, line_width=-1, color=None, zorder=None):
+    def setting(self, fig_size=-1, mark_size=-1, line_width=-1, link_alpha=-1, color=None, zorder=None):
         if fig_size != -1:
             self.leg_shape.fig_size = fig_size
         if mark_size != -1:
             self.leg_shape.mark_size = mark_size
         if line_width != -1:
             self.leg_shape.line_width = line_width
+        if link_alpha != -1:
+            self.leg_shape.link_alpha = link_alpha
         if color != None:
             self.leg_shape.color = color
         if zorder != None:
             self.leg_shape.zorder = zorder
+
             
     #### Plot leg with current shape ####
     def plot_leg(self, theta, beta, O, ax):
-        # initialize all graphics 
+        # Initialize all graphics 
         self.forward(theta, beta, vector=False)  # update to apply displacement of origin of leg.
-        self.leg_shape.get_shape()
-        self.leg_shape.set_shape(O)  # set to apply displacement of origin of leg.
+        self.leg_shape.get_shape(O)
         # self.center_line, = ax.plot([], [], linestyle='--', color='blue', linewidth=1)   # center line (Axis of Symmetry)
-        self.joint_points = [ ax.plot([], [], marker='o', color=self.leg_shape.color, markersize=self.leg_shape.mark_size, zorder=self.leg_shape.zorder)[0] for _ in range(5) ]   # five dots at the center of joints
-        # add leg part to the plot
+        # Add leg part to the plot
         for key, value in self.leg_shape.__dict__.items():
             if "rim" in key:
                 ax.add_patch(value.arc[0])
                 ax.add_patch(value.arc[1])
+                if self.leg_shape.link_alpha > 0:
+                    ax.add_patch(value.arc_fill)
             elif "joint" in key:
                 ax.add_patch(value)
             elif "bar" in key:
                 ax.add_line(value)
-        # joint points
+        # Joint points
+        self.joint_points = [ ax.plot([], [], marker='o', color=self.leg_shape.color, markersize=self.leg_shape.mark_size, zorder=self.leg_shape.zorder+0.00001)[0] for _ in range(5) ]   # five dots at the center of joints
         for i, circle in enumerate([self.leg_shape.upper_joint_r, self.leg_shape.upper_joint_l, self.leg_shape.lower_joint_r, self.leg_shape.lower_joint_l, self.leg_shape.G_joint]):
             center = circle.get_center()
             self.joint_points[i].set_data([center[0]], [center[1]])
@@ -147,7 +124,7 @@ class PlotLeg(LegModel.LegModel):
         O = np.array(O)
         if ax is None:
             fig, ax = plt.subplots()
-        # plot setting
+        # Plot setting
         ax.set_aspect('equal')  # 座標比例相同
         ax = self.plot_leg(theta, beta, O, ax)
         return ax
